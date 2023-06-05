3fd5b024bff4dea47b8e6119dd11cd1e4cf86803
Queiroz Santana <alintana18@gmail.com>
2023-06-05 20:01:00 +0100

    funcoes, primeiro commit

---
+++ bin/funcoes.dart
@@ -0,0 +1,5 @@
+import 'package:funcoes/funcoes.dart' as funcoes;
+
+void main(List<String> arguments) {
+  print('Hello world: ${funcoes.calculate()}!');
+}
---
+++ lib/funcoes/funcoes1.dart
@@ -0,0 +1,7 @@
+void main() {
+  print('A soma de é ${somaDoublesDefault()}');
+}
+
+double somaDoublesDefault({double num1 = 0.1, double num2 = 1.2}) {
+  return num1 + num2;
+}
